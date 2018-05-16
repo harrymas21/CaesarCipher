8b6ad21a1d65ad99dc67ca730f91508ec55aa807
Harry Masidza <harrymas21@gmail.com>
2018-05-16 17:52:42 +0300

    cipher_utility added

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CaesarCipher.Utils;
 
 namespace CaesarCipher
 {
@@ -10,6 +11,29 @@ namespace CaesarCipher
     {
         static void Main(string[] args)
         {
+            CipherUtility cipherUtility = new CipherUtility();
+
+            Console.WriteLine("Type a string to encrypt: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            Console.Write("Enter your Key: ");
+            int key = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Encrypted Data");
+
+            string cipherText = cipherUtility.Encipher(userInput, key);
+            Console.WriteLine(cipherText);
+            Console.Write("\n");
+
+            Console.WriteLine("Decrypted Data: ");
+
+            string t = cipherUtility.Decipher(cipherText, key);
+            Console.WriteLine(t);
+            Console.Write("\n");
+
+            Console.ReadKey();
         }
     }
 }
